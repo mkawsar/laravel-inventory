adc32f23f9e45241185be508bc2a1821b314a1ea
mkawsar <kawsar.diu888@gmail.com>
2023-01-18 22:25:38 +0600

    added login page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
-    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
     @inertiaHead
 </head>
 
 <body>
 @inertia
+<script src="{{ asset(mix('js/app.js')) }}" defer></script>
 </body>
-
 </html>
