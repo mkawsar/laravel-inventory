fa0a6c781acc37ca7fe2ab5a06ddf2f013b78b6d
mkawsar <kawsar.diu888@gmail.com>
2023-01-19 10:16:52 +0600

    added login form validation with error message display

--- resources/js/Pages/auth/Login.vue
+++ resources/js/Pages/auth/Login.vue
@@ -15,7 +15,7 @@
                 </div>
             </div>
             <div class="mt-10">
-                <form action="#">
+                <form method="post" @submit.prevent="submit">
                     <div class="flex flex-col mb-6">
                         <label for="email" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">E-Mail Address:</label>
                         <div class="relative">
@@ -28,9 +28,10 @@
                             </div>
                             <input id="email" type="email" name="email"
                                    class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
-                                   placeholder="E-Mail Address"
+                                   placeholder="E-Mail Address" v-model="form.email"
                             />
                         </div>
+                        <span class="text-sm text-red-600" v-text="form.errors.email"></span>
                     </div>
                     <div class="flex flex-col mb-6">
                         <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">Password:</label>
@@ -44,9 +45,10 @@
                             </div>
                             <input id="password" type="password" name="password"
                                    class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
-                                   placeholder="Password"
+                                   placeholder="Password" v-model="form.password"
                             />
                         </div>
+                        <span class="text-sm text-red-600" v-text="form.errors.password"></span>
                     </div>
 
                     <div class="flex items-center mb-6 -mt-4">
@@ -57,7 +59,7 @@
                     </div>
 
                     <div class="flex w-full">
-                        <button type="submit"
+                        <button type="submit" :disabled="form.processing"
                                 class="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in">
                             <span class="mr-2 uppercase">Login</span>
                             <span>
